ce1291da6aa7dacf9761ebd4dfe8d4ad1a9cdc19
Kshamakhopatkar <140256633+Kshamakhopatkar@users.noreply.github.com>
2023-07-22 19:50:54 +0530

    Update streamlit_app.py
    1 line added as breakfast menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
+streamlit.title ('My Parents new healthy dinner!')
+streamlit.title ('Breakfast Menu')
 
+streamlit.title ('My Parents new healthy dinner!')
+streamlit.title ('My Parents new healthy dinner!')
 streamlit.title ('My Parents new healthy dinner!')
